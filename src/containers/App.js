3cc314f4ac0b49249008bc995f235daa4303c6a6
Rouelle Bautista Miranda <miranda.rouelle@gmail.com>
2020-06-21 18:50:10 +0800

    Added redux-thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,41 +5,49 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
     return {
-      searchField: state.searchField
+      searchField: state.searchRobots.searchField,
+      robots: state.requestRobots.robots,
+      isPending: state.requestRobots.isPending,
+      error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      robots: []
-      //removed below due to redux change
-      // robots: [],
-      // searchField: ''
-    }
-    //console.log('Constructor');
-  }
+  //removed below due to redux change
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //
+  //     // robots: [],
+  //     // searchField: ''
+  //   }
+  //   //console.log('Constructor');
+  // }
 
   componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}));
-
-      //this.setState({robots: robots});
-      //console.log('componentDidMount');
+    this.props.onRequestRobots();
   }
+  // Removed due to react-redux/thunk
+  // componentDidMount(){
+  //     fetch('https://jsonplaceholder.typicode.com/users')
+  //       .then(response => response.json())
+  //       .then(users => this.setState({robots: users}));
+  //
+  //     //this.setState({robots: robots});
+  //     //console.log('componentDidMount');
+  // }
 
   // Removed due to react-redux
   // onSearchChange = (event) => {
@@ -48,13 +56,13 @@ class App extends Component{
 
 
   render(){
-    const { robots } = this.state;
-    const {searchField, onSearchChange} = this.props;
+    // const { robots } = this.state;
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     const filteredRobots = robots.filter(robot =>  {
         return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
     //console.log('render');
-    return !robots.length ?
+    return isPending ?
       (<h1>Loading</h1>) :
       (
         <div className='tc'>
@@ -68,7 +76,6 @@ class App extends Component{
         </div>
       )
   };
-
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,7 @@
-import {CHANGE_SEARCH_FIELD} from './constants.js';
+import { CHANGE_SEARCH_FIELD,
+         REQUEST_ROBOTS_PENDING,
+         REQUEST_ROBOTS_SUCCESS,
+         REQUEST_ROBOTS_FAILED } from './constants.js';
 
 const initialState = {
   searchField: ''
@@ -9,6 +12,27 @@ export const searchRobots = (state=initialState, action={}) => {
     case CHANGE_SEARCH_FIELD:
       //return { ...state, {searchField: action.payload})}
       return Object.assign({}, state, {searchField: action.payload});
+    default:
+      return state;
+  }
+}
+
+const initialSateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+}
+
+export const requestRobots = (state=initialSateRobots, action={}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, { isPending: true });
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, { robots: action.payload,
+                                        isPending: false });
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, { error: action.payload,
+                                        isPending: false });
     default:
       return state;
   }
